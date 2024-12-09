c9ed051b1a6c7245138a2b46ddc2abf4e4fbca07
iammanikandanp <168256488+iammanikandanp@users.noreply.github.com>
2024-12-09 06:50:56 +0530

    Add files via upload

---
+++ protfilo/main.js
@@ -0,0 +1,10 @@
+var options = {
+    strings:["Frontend Developer","Web Developer","MERN Stack Developer","Backend Developer"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true
+};
+
+var typed = new Typed("#typed-output", options)
